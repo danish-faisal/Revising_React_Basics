95622d0eecee9d3a830d5bccc74bd84c58cf08af
Danish Faisal <danishfaisal.wwe@gmail.com>
2022-10-22 12:32:10 +0530

    Tailwind CSS setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import CounterExample from "./components/CounterExample";
+import Header from "./components/Header";
 import HelloWorld from "./components/HelloWorld";
 
 function App() {
   return (
     <div>
+      <Header />
       <HelloWorld name={"Danish"} />
       <CounterExample />
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+  return <header>App Name</header>;
+};
+
+export default Header;
